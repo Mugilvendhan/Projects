1eba2e2f26c6fbaca4047d622395b081e7072320
Mugilvendhan <105462775+Mugilvendhan@users.noreply.github.com>
2023-10-09 16:29:51 +0530

    Update Array.sol

--- SOLIDITY PROJECTS/Smart Contract/Array.sol
+++ SOLIDITY PROJECTS/Smart Contract/Array.sol
@@ -1,1 +1,17 @@
+pragma solidity ^0.8.17;
+contract demo{
+    uint[5] public arr=[10,20,30,40,50];
+
+function insert(uint index,uint _item) public{
+    arr[index]=_item;
+}
+
+function returnArr(uint index) public view returns(uint){
+    return arr[index];
+}
+
+function returnAllelements() public view returns(uint[5] memory) {
+    return arr;
+}
+}
 
